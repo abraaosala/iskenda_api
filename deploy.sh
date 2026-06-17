4e7ca0bee05ca53d07c63727b5663105dc9ccd59
Abraão <abraao@iskenda.com.br>
2026-06-17 07:07:10 +0100

    choco: Alterando o deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ composer install --no-dev --optimize-autoloader --no-interaction
 
 php artisan migrate --force
 
-php artisan config:cache
-php artisan route:cache
+# php artisan config:cache
+# php artisan route:cache
 php artisan view:cache
 php artisan optimize
 
